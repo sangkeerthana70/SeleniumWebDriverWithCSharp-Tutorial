6cebdab818316be3fa601aad51d31e63a43d4bc3
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-23 12:39:26 -0400

    made changes to the setup

--- SetUpEnvironment/EntryPoint.cs
+++ SetUpEnvironment/EntryPoint.cs
@@ -7,7 +7,7 @@ class EntryPoint
 {
     static void Main()
     {
-        /*
+
         IWebDriver driver = new ChromeDriver();
 
         driver.Navigate().GoToUrl("http://testing.todvachev.com/");
@@ -15,40 +15,7 @@ class EntryPoint
         Thread.Sleep(5000);
 
         driver.Quit();
-        */
-
-        IWebDriver driver = new ChromeDriver();
-
-        driver.Navigate().GoToUrl("http://testing.todvachev.com/selectors/name/");
-
-        IWebElement element = driver.FindElement(By.Name("myName"));
-
-        driver.Manage().Window.Maximize();
-
-        //displays a boolean if element is visible or not
-        if (element.Displayed)
-        {
-            GreenMessage("Element is visible");
-        }
-        else
-        {
-            RedMessage("Element not visible, something went wrong");
-        }
-        Thread.Sleep(5000);
-        driver.Quit();
     }
+        
 
-    private static void RedMessage(string message)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(message);
-        Console.ForegroundColor = ConsoleColor.White;
-    }
-
-    private static void GreenMessage(string message)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine(message);
-        Console.ForegroundColor = ConsoleColor.White;
-    }
 }
