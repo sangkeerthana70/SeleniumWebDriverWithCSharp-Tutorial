d183a60a4186fcdbb45f81aa8638512818bb5ac4
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-29 17:39:24 -0400

    Few IWebdriver commands to navigate to a browser

---
+++ IWebDriverBrowserCommands/IWebDriverBrowserCommands/SeleniumBasics.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace DriverCommands
+{
+    class SeleniumBasics
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://demoqa.com/frames-and-windows");
+
+            driver.FindElement(By.XPath(".//*[@id='tabs-1']/div/p/a")).Click();
+            driver.Close();
+        }
+    }
+}
